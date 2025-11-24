7d4eaa8c59c6df65506277ec4e6d2a90a893dc57
Abhinav Yadav <abhinavyadav.ay17@gmail.com>
2025-11-24 14:55:33 +0530

    updated

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -3,7 +3,7 @@ export const eventsData = [
     id: 1,
     title: "Git and GitHub Workshop",
     date: "2025-11-24",
-    venue: "Audi-2",
+    venue: "Audi-1 Room No.322",
     description:
       "Learn Git and GitHub fundamentals with hands-on labs and win exciting prizes.",
     fullDescription:
